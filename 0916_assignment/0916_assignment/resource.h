11bd357c7ca1aaca801e77fb571cd2f1f0f41bc6
202407030 <super8114@yuhan.ac.kr>
2025-11-30 23:31:41 +0900

    4주차 그림판 과제 업로드

---
+++ 0916_assignment/0916_assignment/resource.h
@@ -0,0 +1,40 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// 0916_assignment.rc에서 사용되고 있습니다.
+//
+#define IDC_MYICON                      2
+#define IDD_MY0916ASSIGNMENT_DIALOG     102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_MY0916ASSIGNMENT            107
+#define IDI_SMALL                       108
+#define IDC_MY0916ASSIGNMENT            109
+#define IDR_MAINFRAME                   128
+#define ID_LINE                         32784
+#define ID_RACT                         32785
+#define ID_ELS                          32786
+#define ID_FREE                         32787
+#define ID_1PX                          32788
+#define ID_5PX                          32789
+#define ID_10PX                         32790
+#define ID_ST_RED                       32791
+#define ID_ST_GREED                     32792
+#define ID_ST_BLUE                      32793
+#define ID_BRUSH_BLACK                  32794
+#define ID_BRUSH_WHITE                  32795
+#define ID_BRUSH_PURPLE                 32796
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32797
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
